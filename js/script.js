8015fe70e92e74056180546bdbc1a4e4149e06ac
SK's Laptop <karthiksaladi6@gmail.com>
2024-08-02 22:50:51 +0530

    responsive completed for 768px

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /*========== menu icon navbar ==========*/
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
@@ -22,9 +26,13 @@ window.onscroll = () => {
   });
   /*========== sticky navbar ==========*/
   let header = document.querySelector(".header");
+
   header.classList.toggle("sticky", window.scrollY > 100);
 
   /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+  
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 /*========== swiper ==========*/
